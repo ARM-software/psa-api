190fab83235488dddb25b96eeaee5d6c65142c84
Andrew Thoelke <andrew.thoelke@arm.com>
2023-03-23 23:29:10 +0000

    Configure for publishing Crypto 1.1.2

--- doc/crypto/conf.py
+++ doc/crypto/conf.py
@@ -30,7 +30,7 @@ doc_info = {
     'issue_no': 2,
     # Identifies the sequence number of a release candidate of the same issue
     # default to None
-    'release_candidate': 1,
+    'release_candidate': None,
     # Draft status - use this to indicate the document is not ready for publication
     #'draft': True,
 
@@ -43,7 +43,7 @@ doc_info = {
     'license': 'psa-certified-api-license',
 
     # Document date, default to build date
-    #'date': '17/10/2022',
+    'date': '23/03/2022',
 
     # Default header file for API definitions
     # default to None, and can be set in documentation source
